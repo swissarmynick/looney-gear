1855086e63f3f5bd29b20103cd5baaee92027b64
Nicholas Looney <swissarmynick@gmail.com>
2022-03-15 12:26:31 -0500

    Course changed. Setup using category-item and directory folders. Removed menu-item and homepage configuration. Working as before.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import './App.css';
+import Directory from "./components/directory/directory.component";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      linkUrl: 'shop/hats'
+    },
+    {
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      linkUrl: 'shop/jackets'
+    },
+    {
+      id: 3,
+      title: 'shoes',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      linkUrl: 'shop/sneakers'
+    },
+    {
+      id: 4,
+      title: 'women',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      size: 'large',
+      linkUrl: 'shop/womens'
+    },
+    {
+      id: 5,
+      title: 'men',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      linkUrl: 'shop/mens'
+    }
+  ]
 
-function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Directory categories={categories}/>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,65 +1,20 @@
-import React from "react";
-import MenuItem from '../menu-item/menu-item.component';
+import CategoryItem from "../category-item/category-item.component";
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            sections: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'shoes',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'women',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'men',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-              ]
-        }
-    }
 
-    render() { 
-        return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({ title, imageUrl, size, id, }) => (
-                        <MenuItem 
-                            title={title}
-                            imageUrl={imageUrl}
-                            size={size}
-                            key={id}
-                        />
-                    ))
-                }
-            </div>
-        )
-    }
+const Directory = ({ categories }) => {
+  return ( 
+    <div className="directory-container">
+      {
+        categories.map(category => (
+          <CategoryItem 
+            key={category.id}
+            category={category}  
+          />
+        )) 
+      }
+    </div>
+   );
 }
  
 export default Directory;
