c82e9606adb7df320800ccfcae928a02aa2f2595
Nicholas Looney <swissarmynick@gmail.com>
2022-03-23 10:53:56 -0500

    Converted Navigation and Buttons over to using styled components. Not sure if it really makes sense to convert the rest of the app this way. Feels like overkill for the sake of teaching styled components. The apps scss structure makes sense as is. Moving forward but documenting this reference point.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,16 @@
 
 import { useContext } from 'react';
-import { Link, Outlet } from "react-router-dom";
-
-import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { Outlet } from "react-router-dom";
 
 import { UserContext } from '../../context/user.context';
 import { CartContext } from '../../context/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import './navigation.styles.scss';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+import { LogoContainer, NavigationContainer, NavLinks, NavLink } from './navigation.styles';
 
 const Navigation = () => {
 
@@ -19,32 +19,20 @@ const Navigation = () => {
 
     return (
       <>
-        <div className="navigation">
-            <Link className="logo-container" to='/'>
+        <NavigationContainer>
+            <LogoContainer to='/'>
                 <CrownLogo className="logo"/>
-            </Link>
-            <div className="nav-links-container">
-                <Link className="nav-link" to='/shop'>
-                    SHOP
-                </Link>
-
-                {currentUser ? (
-                    <span className='nav-link' onClick={signOutUser}>
-                        SIGN OUT
-                    </span>
-                    )  
-                    : (
-                        <Link className="nav-link" to='/auth'>
-                            SIGN IN
-                        </Link>
-                    )
+            </LogoContainer>
+            <NavLinks>
+                <NavLink to='/shop'>SHOP</NavLink>
+                {currentUser 
+                    ? (<NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>)  
+                    : (<NavLink to='/auth'>SIGN IN</NavLink>)
                 }
                 <CartIcon />
-            </div>
-            {
-                isCartOpen && <CartDropdown />
-            }
-        </div>
+            </NavLinks>
+            {isCartOpen && <CartDropdown />}
+        </NavigationContainer>
         <Outlet />
       </>
     )
