926294000e1eece6438bf7a21d1ef87dc6de8617
Nicholas Looney <swissarmynick@gmail.com>
2022-03-31 18:26:15 -0500

    Integrated with Stripe via Netlify serverless function. Setup netlify folder and env file at root level. Create PaymentForm. Touched up button styling. Section 18 complete.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './store/store';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
+import { persistor, store } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
 import './index.scss';
 
 ReactDOM.render(
@@ -13,7 +16,9 @@ ReactDOM.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,4 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+// export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+export const stripePromise = loadStripe("pk_test_51KjVETHRCBzhAjiAwG7So2ciX86WeavlJBJnnHrznEVGexplZ7mxxFKI8994F5CkJkTNLTAkfOkJbkI1fmLbqZ8c00z5zlfOna");
